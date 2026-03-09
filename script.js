95af4aa2bbe5229c0650d24e4730927265d757e9
AnshumanRaj2011 <nibha120416@gmail.com>
2026-03-09 22:12:54 +0530

    Add like button toggle functionality

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ document.getElementById("profile").innerHTML =
 "Viewing profile: " + username
 
 }
+
+
+
+let likes = document.querySelectorAll(".like")
+
+likes.forEach(button => {
+
+button.addEventListener("click", () => {
+
+if(button.innerHTML === "❤️"){
+
+button.innerHTML = "💔"
+
+}else{
+
+button.innerHTML = "❤️"
+
+}
+
+})
+
+})
