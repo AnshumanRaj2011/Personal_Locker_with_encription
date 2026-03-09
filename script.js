62dda783135e438368c7a67281b43fc1bd5d2911
AnshumanRaj2011 <nibha120416@gmail.com>
2026-03-09 22:01:47 +0530

    Rename search function to searchProfile

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function search(){
+function searchProfile(){
 
 let username = document.getElementById("username").value
 
-document.getElementById("results").innerHTML =
-"Searching for " + username
+document.getElementById("profile").innerHTML =
+"Viewing public profile: " + username
+
+
+let reels = `
+
+<video controls autoplay loop>
+<source src="https://www.w3schools.com/html/mov_bbb.mp4">
+</video>
+
+<video controls autoplay loop>
+<source src="https://www.w3schools.com/html/movie.mp4">
+</video>
+
+`
+
+
+document.getElementById("reels").innerHTML = reels
 
 }
