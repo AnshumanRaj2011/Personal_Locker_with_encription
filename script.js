a3864aafff9aaa292f15f83e64e57d6267f3a659
AnshumanRaj2011 <nibha120416@gmail.com>
2026-03-09 22:07:01 +0530

    Fix profile display message and clean up code

--- script.js
+++ script.js
@@ -3,22 +3,6 @@ function searchProfile(){
 let username = document.getElementById("username").value
 
 document.getElementById("profile").innerHTML =
-"Viewing public profile: " + username
-
-
-let reels = `
-
-<video controls autoplay loop>
-<source src="https://www.w3schools.com/html/mov_bbb.mp4">
-</video>
-
-<video controls autoplay loop>
-<source src="https://www.w3schools.com/html/movie.mp4">
-</video>
-
-`
-
-
-document.getElementById("reels").innerHTML = reels
+"Viewing profile: " + username
 
 }
